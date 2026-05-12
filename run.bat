2b8e7c5203641e7f9bd89ae8bb3a4e0a11a70fb8
diegemiao <189215347@qq.com>
2026-05-12 21:02:24 +0800

    use gh API push

--- run.bat
+++ run.bat
@@ -2,13 +2,9 @@
 cd /d C:\Users\win11\Desktop\news
 node scripts\main.js
 echo.
-echo git push...
-git stash
-git pull --rebase origin master
-git stash pop
-git add data\archive.json docs\index.html
-git commit -m "manual push: news update"
-git push origin master
-echo.
+echo Pushing to GitHub...
+for /f "delims=" %%i in ('"C:\Program Files\GitHub CLI\gh.exe" auth token') do set GH_TOKEN=%%i
+set GH_TOKEN=%GH_TOKEN%
+node scripts\push.js
 echo Done
 pause
