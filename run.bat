501c8d02ef959fa9d039024c25933ef191070f59
diegemiao <189215347@qq.com>
2026-05-12 21:08:18 +0800

    simplify run.bat

--- run.bat
+++ run.bat
@@ -3,8 +3,6 @@ cd /d C:\Users\win11\Desktop\news
 node scripts\main.js
 echo.
 echo Pushing to GitHub...
-for /f "delims=" %%i in ('"C:\Program Files\GitHub CLI\gh.exe" auth token') do set GH_TOKEN=%%i
-set GH_TOKEN=%GH_TOKEN%
 node scripts\push.js
 echo Done
 pause
